f0bd5f8c0eca36e2e2e875b07ccd5fd144867067
iou2much <iou2much@sina.com>
2021-06-23 09:05:25 +0800

    update

--- js/around.js
+++ js/around.js
@@ -2,7 +2,7 @@
 $(function() {
 	// var BK_URL = "http://10.22.32.15:8170/cn_api";
 	var BK_URL = "http://pin.test.seewo.com/pin/cn_api";
-	var BK_URL = "http://ds.seewo.com//dm/pin/cn_api/get_info";
+	var BK_URL = "http://ds.seewo.com/dm/pin/cn_api";
 
 	$('.inp').focus(function(){
 		if($(this).val() == $(this).attr('tip')){
